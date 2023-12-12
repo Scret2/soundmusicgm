eb2b8e55331343a1d23c51dd123862b1e4cf1195
Hyogo <106125029+Scret2@users.noreply.github.com>
2023-12-12 12:12:28 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,16 @@ console.log("main.js!!");
 let counter = 0;
 let interid = -1; //ここで-1を入れているからif文で0以下しか実行出来ない場合に対応出来る。
 let snd1 = null; //サウンド１
+let soundicon = $("#soundicon");
+let dropdownContent = $("#dropdownContent");
 // Ready
-$(document).ready(() => {});
+$(document).ready(() => {
+  soundicon.on("mouseover", function () {
+    // クリックされたときに行いたいアクションをここに記述
+    dropdownContent.addClass("show");
+  });
+
+  dropdownContent.on("mouseleave", function () {
+    dropdownContent.removeClass("show");
+  });
+});
