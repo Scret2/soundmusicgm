5ae96d9b2626a6a7fc5ff5e00ff2252a33d9fca4
Hyogo <106125029+Scret2@users.noreply.github.com>
2023-12-14 12:12:02 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
 console.log("main.js!!");
 
 let counter = 0;
-let interid = -1; //ここで-1を入れているからif文で0以下しか実行出来ない場合に対応出来る。
-let snd1 = null; //サウンド１
+let interid = -1;
+let snd1 = null;
+let snd2 = null;
+let snd3 = null;
+let snd4 = null;
+let snd5 = null;
 let soundicon = $("#soundicon");
 let dropdownContent = $("#dropdownContent");
+
+const sounds = [
+  "./sounds/Mitsukiyo Constant Moderato.mp3",
+  "./sounds/Mitsukiyo Unwelcome School.mp3",
+  "./sounds/Mitsukiyo Romantic Smile.mp3",
+  "./sounds/Mitsukiyo Lovely Picnic.mp3",
+  "./sounds/Nor Aoharu.mp3",
+];
+
+const howls = [];
+
+// Initialize Howl instances
+for (let i = 0; i < sounds.length; i++) {
+  howls[i] = new Howl({
+    src: [sounds[i]],
+    loop: true,
+    volume: 1.0,
+    autoplay: false,
+  });
+
+  $(`#music${i + 1}`).click(() => {
+    // Check if the sound is already playing
+    if (!howls[i].playing()) {
+      // Stop all other sounds
+      for (let j = 0; j < howls.length; j++) {
+        if (j !== i && howls[j].playing()) {
+          howls[j].stop();
+        }
+      }
+      // Play the selected sound
+      howls[i].play();
+    }
+  });
+}
+
 // Ready
 $(document).ready(() => {
   soundicon.on("mouseover", function () {
-    // クリックされたときに行いたいアクションをここに記述
     dropdownContent.addClass("show");
   });
 
